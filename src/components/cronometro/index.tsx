faf760b9398c73b585581973147d76c56dc121e3
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-29 09:44:01 -0300

    Módulo cronômetro

---
+++ src/components/cronometro/index.tsx
@@ -0,0 +1,17 @@
+import Botao from "../botão";
+import style from "./Cronometro.module.scss";
+import Relogio from "./relogio";
+
+export default function Cronometro() {
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um cars e inicie o cronômetro</p>
+
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+
+      <Botao>Começar</Botao>
+    </div>
+  );
+}
