75beb85ee9d146ad7985b8696bc99a3c4f3c3543
Bruno Rocha de Lima <brunorocha33@live.com>
2022-04-02 19:37:56 -0300

    Finalizando css

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import style from "../pages/App.module.scss";
 import "../pages/reset.scss";
 import { ITarefa } from "../../types/tarefa";
-
+import {tempoParaSegundos} from "../../common/utils/date";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
@@ -13,7 +13,13 @@ function App() {
 
   function selecionaTarefa(tarefaSelecionada: ITarefa){
     setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa =>({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
   }
+
+  tempoParaSegundos("02:05:03")
 
   return (
     <div className={style.AppStyle}>
